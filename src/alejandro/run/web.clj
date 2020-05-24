fab3cf9a0c5b20c18963c88142efe680bc06b6c3
Alejandro Gomez <bandarra@protonmail.com>
2020-05-24 15:59:48 +0200

    Clean CSS and HTML a bit

--- src/alejandro/run/web.clj
+++ src/alejandro/run/web.clj
@@ -19,24 +19,24 @@
 
 (defn navbar
   [page meta]
-  [:navbar.navbar.sans.centered
-   [:ul.nav-links
-    [:li.home
-     [:a {:href "/"}
-      [:span.paren "("]
-      (:site-title meta)
-      [:span.paren ")"]]]
-    [:li
-     (case page
-       :blog [:a.current-page {:href "/index.html"} "blog"]
-       [:a {:href "/index.html"} "blog"])]
-    [:li
-     (case page
-       :about [:a.current-page {:href "/about.html"} "about"]
-       [:a {:href "/about.html"} "about"])]]
+  [:header.navbar.sans.centered
+   [:nav
+    [:ul.nav-links
+     [:li.home
+      [:a {:href "/"}
+       [:span.paren "("]
+       (:site-title meta)
+       [:span.paren ")"]]]
+     [:li
+      (case page
+        :blog [:a.current-page {:href "/index.html"} "blog"]
+        [:a {:href "/index.html"} "blog"])]
+     [:li
+      (case page
+        :about [:a.current-page {:href "/about.html"} "about"]
+        [:a {:href "/about.html"} "about"])]]]
    [:ul.external-links
-    [:li.icon-link]
-    [:li.icon-link
+    [:li
      [:a {:href "http://twitter.com/pvrrgrammer"}
       [:svg
        {:title "Twitter"
@@ -45,7 +45,7 @@
         :fill "#00AACE"
         :viewBox "0 0 24 24"}
        [:path {:d "M23.954 4.569c-.885.389-1.83.654-2.825.775 1.014-.611 1.794-1.574 2.163-2.723-.951.555-2.005.959-3.127 1.184-.896-.959-2.173-1.559-3.591-1.559-2.717 0-4.92 2.203-4.92 4.917 0 .39.045.765.127 1.124C7.691 8.094 4.066 6.13 1.64 3.161c-.427.722-.666 1.561-.666 2.475 0 1.71.87 3.213 2.188 4.096-.807-.026-1.566-.248-2.228-.616v.061c0 2.385 1.693 4.374 3.946 4.827-.413.111-.849.171-1.296.171-.314 0-.615-.03-.916-.086.631 1.953 2.445 3.377 4.604 3.417-1.68 1.319-3.809 2.105-6.102 2.105-.39 0-.779-.023-1.17-.067 2.189 1.394 4.768 2.209 7.557 2.209 9.054 0 13.999-7.496 13.999-13.986 0-.209 0-.42-.015-.63.961-.689 1.8-1.56 2.46-2.548l-.047-.02z"}]]]]
-    [:li.icon-link
+    [:li
      [:a {:href "http://github.com/purrgrammer"}
       [:svg
        {:title "Github"
@@ -150,15 +150,15 @@
   (html {:title (str (:site-title global-meta) " | " (:title post))
          :meta global-meta}
         [:article
-         [:h1 (:title post)]
+         [:h1.article-title
+          (:title post)]
          [:div.article-meta.sans
-          [:div.tags
-           "tags: "
-           (interpose ", "
-                      (map #(tag->link global-meta %) (:tags post)))]
-          [:span.ttr
+          [:span.article-ttr
            "&#128337;"
-           (time-to-read post)]]
+           (time-to-read post)]
+          [:div.tags
+           (interpose ", "
+                      (map #(tag->link global-meta %) (:tags post)))]]
          (:content post)]))
 
 (defn tag
